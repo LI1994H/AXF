0a30a78694ab65524c9cb5d4a181166be1e31377
LH1994H <784155823@qq.com>
2018-11-01 15:45:05 +0800

    轮播模型创建

--- AXF/models.py
+++ AXF/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Base(models.Model):
+    img = models.CharField(max_length=100)  # 图片
+    name = models.CharField(max_length=180)  # 名称
+    trackid = models.CharField(max_length=10)  # 商品编号
+
+    class Meta:
+        abstract = True  # 抽象化
+
+class Wheel(Base):
+    class Meta:
+        db_table = 'axf_wheel'  # 修改表名
