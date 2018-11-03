ade5950f29b4909e675bd2f30ad5f46bde4b88c4
LH1994H <784155823@qq.com>
2018-11-03 11:28:50 +0800

    使用cookie记录每次点击分类的位置

---
+++ static/market/js/market.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.market').width(innerWidth);
+    // 每次获取 上次点击分类的位置
+    typeIndex = $.cookie('typeIndex');
+    if (typeIndex){
+        $('.type-slider .type-item').eq(typeIndex).addClass('active')
+    } else {
+
+        $('.type-slider .type-item:first').addClass('active')
+    }
+    // 每次点击记录点击分类的位置
+    $('.type-item').click(function () {
+       $.cookie('typeIndex', $(this).index(), {expires:3, path:'/'})
+    })
+
+});
